c2da660f87d05bdefa39e93b45d510af5c832db1
Floris Bruynooghe <flub@devork.be>
2012-02-24 01:37:21 +0000

    Add a license and trove classifiers

--- setup.py
+++ setup.py
@@ -8,6 +8,17 @@ setup(name='pytest-timeout',
       author='Floris Bruynooghe',
       author_email='flub@devork.be',
       url='http://bitbucket.org/flub/pytest-timeout/',
+      license='MIT',
       py_modules=['pytest_timeout'],
       entry_points={'pytest11': ['timeout = pytest_timeout']},
-      install_requires=['pytest>=2.0'])
+      install_requires=['pytest>=2.0'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: DFSG approved',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing'])
