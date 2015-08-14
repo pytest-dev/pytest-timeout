d0d0233259bed031e1ee8950b065543d8d2446e7
Floris Bruynooghe <flub@devork.be>
2015-08-15 00:43:51 +0100

    Set version to 0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='pytest-timeout',
     description='py.test plugin to abort hanging tests',
     long_description=open("README").read(),
-    version='0.5dev0',
+    version='0.5',
     author='Floris Bruynooghe',
     author_email='flub@devork.be',
     url='http://bitbucket.org/pytest-dev/pytest-timeout/',
