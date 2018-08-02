03610b4da99d873a202f406097c54fba7ec96d07
loader <loader@freebsd.org>
2018-08-02 09:07:36 +0000

    Use UTF-8 encoding to open the README file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import io
 setup(
     name='pytest-timeout',
     description='py.test plugin to abort hanging tests',
-    long_description=io.open("README", encoding="utf-8").read(),
+    long_description=io.open('README', encoding='utf-8').read(),
     version='1.3.1',
     author='Floris Bruynooghe',
     author_email='flub@devork.be',
