b8f93a92aea888a0cea43632451ef8ea27a30507
Matt Marcus <mattmarcus58@gmail.com>
2020-04-15 17:36:04 -0600

    fix: formatting

--- pytest_timeout.py
+++ pytest_timeout.py
@@ -137,7 +137,7 @@ def pytest_enter_pdb():
 
 def is_debugging():
     """
-    Detects if a debugging session is in progress by checking if either of two conditions is true. 
+    Detects if a debugging session is in progress by checking if either of two conditions is true.
         1. examining the stack frames to see if pydevd.py — 
      a debugging framework used by VSCode, PyCharm, and others — is present.
         2. Check is SUPPRESS_TIMEOUT is set to True
