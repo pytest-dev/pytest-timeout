a65697430a8027906ac8d2d3f2f4e12f95a1ffd9
pctablet505 <pctablet505@gmail.com>
2026-07-15 19:09:46 +0000

    fix: read timeout_disable_debugger_detection from ini file
    Fixes #175

--- test_pytest_timeout.py
+++ test_pytest_timeout.py
@@ -526,6 +526,54 @@ def test_disable_debugger_detection_flag(
     assert "fail" in result
 
 
+@pytest.mark.parametrize(
+    ("debugging_module", "debugging_set_trace"),
+    [
+        ("pdb", "set_trace()"),
+        pytest.param(
+            "ipdb",
+            "set_trace()",
+            marks=pytest.mark.xfail(
+                reason="waiting on https://github.com/pytest-dev/pytest/pull/7207"
+                " to allow proper testing"
+            ),
+        ),
+        pytest.param(
+            "pydevd",
+            "settrace(port=4678)",
+            marks=pytest.mark.xfail(reason="in need of way to setup pydevd server"),
+        ),
+    ],
+)
+@have_spawn
+def test_disable_debugger_detection_ini(
+    pytester, debugging_module, debugging_set_trace
+):
+    pytester.makepyfile(
+        f"""
+        import pytest, {debugging_module}
+
+        @pytest.mark.timeout(1)
+        def test_foo():
+            {debugging_module}.{debugging_set_trace}
+    """
+    )
+    pytester.makeini(
+        """
+        [pytest]
+        timeout_disable_debugger_detection = true
+    """
+    )
+    child = pytester.spawn_pytest(str(pytester.path))
+    child.expect("test_foo")
+    time.sleep(1.2)
+    result = child.read().decode().lower()
+    if child.isalive():
+        child.terminate(force=True)
+    assert "timeout (>1.0s)" in result
+    assert "fail" in result
+
+
 def test_is_debugging(monkeypatch):
     import pytest_timeout
 
